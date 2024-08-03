088499377d2a4dfec74d07d5dd261e40afa22674
boykodmitryv <boykodmitryv@mail.ru>
2024-08-03 22:02:45 +0300

    multiply.js

---
+++ 8kyu/multiply.js
@@ -0,0 +1,18 @@
+// Instructions
+// Output
+// Past Solutions
+// This code does not execute properly. Try to figure out why.
+
+// DebuggingFundamentals
+
+
+// function multiply(a, b){
+//     a * b
+//   }
+
+
+//SOLUTION:
+
+  function multiply(a, b){
+    return a * b
+  }
