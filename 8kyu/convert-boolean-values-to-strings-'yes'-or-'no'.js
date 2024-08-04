62b75b7da27811a39ec75f22a77662113787b0f9
boykodmitryv <boykodmitryv@mail.ru>
2024-08-04 22:33:54 +0300

    8kyu\convert-boolean-values-to-strings-'yes'-or-'no'.js

---
+++ 8kyu/convert-boolean-values-to-strings-'yes'-or-'no'.js
@@ -0,0 +1,28 @@
+// 8 kyu
+// Convert boolean values to strings 'Yes' or 'No'.
+// 113329689% of 20,981118,879 of 324,247weavermedia2 Issues Reported
+//  JavaScript
+// Node v18.x
+// VIM
+// EMACS
+// Instructions
+// Output
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// Fundamentals
+
+// function boolToWord( bool ){
+//     //...
+//   }
+
+
+
+
+function boolToWord( bool ){
+    //...
+    if (bool){
+      return "Yes"
+    } else {
+      return "No"
+    }
+  }
