80c5ec40b8343c1b4ee182681f0b31b980508b44
boykodmitryv <boykodmitryv@mail.ru>
2024-09-03 16:12:55 +0300

    8kyu\are-you-playing-banjo.js

---
+++ 8kyu/are-you-playing-banjo.js
@@ -0,0 +1,23 @@
+// 8 kyu
+// Are You Playing Banjo?
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+// function areYouPlayingBanjo(name) {
+
+//   }
+
+  function areYouPlayingBanjo(name) {
+    if (name[0].toLowerCase(0) === "r") {
+      return name + " plays banjo"
+    } else {
+      return name + " does not play banjo"
+    }
+    return name;
+  }
